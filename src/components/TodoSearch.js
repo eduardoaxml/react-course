7baea46a9963a191eafb1ff48fc7c75a4d1e20a6
Pablo Eduardo Merino <3802987+eduardoaxml@users.noreply.github.com>
2023-01-26 13:12:45 -0600

    useState

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -3,14 +3,22 @@ import '../css/TodoSearch.css';
 
 
 function TodoSearch() {
+
+    // ['variable donde guarda el valor', 'funcion para actualizar el valor '] = creamos un estado que por defecto será un valor vacío
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
-    return ( <
-        input className = "TodoSearch"
-        placeholder = "Cebolla"
-        onChange = { onSearchValueChange }
-        />
+    return ( 
+        <input
+            className="TodoSearch"
+            placeholder="Cebolla"
+            value={searchValue}
+            onChange={onSearchValueChange}
+        />,
+        <p>{searchValue}</p>
     );
 }
 export { TodoSearch };
